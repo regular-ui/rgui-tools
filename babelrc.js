4ee41283ce4593d5aff94f0ef18b34de51d5ddd1
Rainfore <rainforest92@126.com>
2017-03-16 18:00:44 +0800

    [fixed] disable babelrc

--- babelrc.js
+++ babelrc.js
@@ -3,11 +3,6 @@
 const fs = require('fs');
 const json = JSON.parse(fs.readFileSync(__dirname + '/.babelrc', 'utf8'));
 json.presets = json.presets.map((preset) => require.resolve('babel-preset-' + preset));
-
-// For IE8
-// "plugins": ["transform-es3-member-expression-literals", "transform-es3-property-literals"]
-// json.plugins = json.plugins.map((plugin) => {
-//     return require.resolve('babel-plugin-' + plugin);
-// });
+json.babelrc = false;
 
 module.exports = json;
